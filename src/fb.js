83d9e96a9f31384ac700100648107c67d567475f
seodong4378 <seodong4378@naver.com>
2021-08-30 21:08:21 +0900

    login part one

--- src/fb.js
+++ src/fb.js
@@ -13,6 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+const authorServie = getAuth();
 
-export default auth;
+export default authorServie;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,38 @@
-import React from "react";
+import { React, useState} from "react";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChage = (event) => {
+        const { target: {name, value}} = event;
+        //console.log(typeof(value))
+        if (name === "email"){
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        } else {
+            //Nothing
+        }
+    }
+
+    const onSubmit = (event) => { 
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="E-mail address" required onChange={onChage} value={email}></input>
+                <input name="password" type="password" placeholder="Password" required onChange={onChage} value={password}></input>
+                <button>Login</button>
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+        );
+}
 
 export default Auth
